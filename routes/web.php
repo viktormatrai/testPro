864cf203fdbf1d9734f0821ff161e802e44b0d10
Viktor Mátrai <viktor.matrai@jofogas.hu>
2024-04-04 17:34:05 +0200

    create show, update, delete routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestCompanyDBController;
 
 Route::post('/company', [TestCompanyDBController::class, 'store']);
-
+Route::get('/company/{companyId}', [TestCompanyDBController::class, 'show']);
+Route::put('/company/{companyId}', [TestCompanyDBController::class, 'update']);
+Route::delete('/company/{companyId}', [TestCompanyDBController::class, 'destroy']);
