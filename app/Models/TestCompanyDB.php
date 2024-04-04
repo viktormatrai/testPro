d62806546177214857a3c686fe7254f7c17a96b2
Viktor Mátrai <viktor.matrai@jofogas.hu>
2024-04-04 19:35:13 +0200

    handle exceptions and fix creating operations

--- app/Models/TestCompanyDB.php
+++ app/Models/TestCompanyDB.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class TestCompanyDB extends Model
